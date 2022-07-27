380bbedf54984174dcd13f760e203a6c05785208
kieiken <110042851+kieiken@users.noreply.github.com>
2022-07-27 09:15:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,11 +11,9 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
